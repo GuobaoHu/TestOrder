5dd703125ec18d3fb381b2eb8d3501a26a4d898b
HuGuobao <398456458@qq.com>
2017-11-23 10:19:15 +0800

    新建订单测试TestOrder项目

---
+++ src/guyue/hu/TestOrder.java
@@ -0,0 +1,14 @@
+package guyue.hu;
+/**
+*@author guyue E-mail:398456458@qq.com
+*version1.0 创建时间:2017年11月23日 上午10:18:17
+*describ 
+*/
+public class TestOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
